7715890f33aa601c77b4d3674db67cbe7900cce2
VolodymyrChoshko <volodymyrchoshko@gmail.com>
2023-04-13 15:46:09 +0100

    step 2

--- src/background.js
+++ src/background.js
@@ -1,4 +1,25 @@
-(function() {
+
+
+(function () {
+  let url = '';
+  let token = '';
+
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    sendResponse({ url, token });
+
+    // sends response to content script
+    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    //   chrome.tabs.sendMessage(tabs[0].id, {"def": "ghi"} );
+    // });
+  });
+  // setInterval(() => {
+  //   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+  //     var tabId = tabs[0].id;
+  //     // Now you can send a message to this tab using tabId
+  //     if(tabId)
+  //       chrome.tabs.sendMessage(tabId, { url, token }, function (response) { });
+  //   });
+  // }, 3000);
   // receives message from popup script
   /* chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     if (request.opened == true) {
@@ -12,27 +33,17 @@
     }
   }); */
 
-  
-//  const browser = window.browser || window.chrome;
-  let url = '';
-  let token = '';
-  let activeTabId;
 
-  chrome.tabs.onActivated.addListener(function(activeInfo) {
-    activeTabId = activeInfo.tabId
-    chrome.tabs.sendMessage(activeTabId, "AAAAAA");
-  });
+  //  const browser = window.browser || window.chrome;
 
   chrome.webRequest.onBeforeSendHeaders.addListener(
-    function(details) {
-      if(details.url.includes("gql.reddit.com"))
-      {
+    function (details) {
+     if (details.url.includes("gql.reddit.com")) {
         // $('#status').text(details.url);
         url = details.url;
 
         console.log(details);
-        if(details.requestHeaders)
-        {
+        if (details.requestHeaders) {
           for (let header of details.requestHeaders) {
             // const hstr = header.name + ' ' + header.value;
             // $('#token').text($('#token').text() + hstr);
@@ -42,14 +53,18 @@
               // $('#token').text(authorizationValue);
               token = authorizationValue;
 
+              // chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+              //   var tabId = tabs[0].id;
+              //   // Now you can send a message to this tab using tabId
+              //   chrome.tabs.sendMessage(tabId, { url, token }, function (response) { });
+              // });
               // chrome.runtime.sendMessage({url, token});
-             chrome.tabs.sendMessage(activeTabId, {url, token}, function(response) {});
               // alert(url, token);
               break;
             }
           }
         }
-      }
+     }
     },
     { urls: ['<all_urls>'] },
     ["blocking", "requestHeaders", "extraHeaders"]
--- src/content.js
+++ src/content.js
@@ -3,17 +3,40 @@
 //   alert(url, token);
 // });
 
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  /* If the received message has the expected format... */
-  alert(msg)
-});
-
 $(document).ready(function () {
+  // chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  //   alert(request)
+  // });
   if ($('a:contains("Log In")').length) {
     // The login button exists, do something
     // $('#status').text('Please log in first');
     alert('Please log in first');
   } else {
+    var details = {
+      'id': 't3_12g8ns5',
+      'dir': '1',
+      'api_type': 'json'
+    };
+
+    var formBody = [];
+    for (var property in details) {
+      var encodedKey = encodeURIComponent(property);
+      var encodedValue = encodeURIComponent(details[property]);
+      formBody.push(encodedKey + "=" + encodedValue);
+    }
+    formBody = formBody.join("&");
+
+    chrome.runtime.sendMessage({ "abc": "def" }, function (response) {
+      fetch('https://oauth.reddit.com/api/vote?redditWebClient=desktop2x&app=desktop2x-client-production&raw_json=1&gilding_detail=1', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+          'Authorization': response.token,
+        },
+        body: formBody
+      });
+      alert(JSON.stringify(response));
+    })
     // $('#status').text('Logged In');
     // The login button does not exist, do something else
     // alert('Logged In');
